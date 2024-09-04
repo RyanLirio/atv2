7ca72beb6bff15692d6c1131b102099483ac33f8
ryan <ryanlirio2@gmail.com>
2024-09-04 20:53:47 -0300

    atv2

---
+++ atv2.cs
@@ -0,0 +1,17 @@
+//jeaggerArray è do Ceara e conhecido como um vetor dos vetores
+
+int [][] numbers = int [5][]
+//Neste caso [][] indica que é
+// um vetor de vetor, neste ex
+// um vetor de 4 posições contendo
+//vetor de tam. indefinido
+
+numbers[0] = new int[] {1,2,3};
+numbers[1] = new int[] {4, 3, 2, 1, 0};
+numbers[2] = new int[] {1, 2, 0};
+numbers[3] = new int[] {57};
+numbers[4] = null!;
+
+bool? teste = null!;
+
+ ConsoleWrite.Line($"")
